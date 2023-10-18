b9a1eaa790f7ef6b12eb0f3007e3a74ca15c54d7
phamngocuyn <uynpham123@gmail.com>
2023-10-18 12:20:46 +0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+function toggleSidebar() {
+    const sidebarTitle = document.querySelector('.sidebar-title');
+    sidebarTitle.classList.toggle('sidebar-open');
+    sidebarTitle.classList.toggle('sidebar-closed');
+}
